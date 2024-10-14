5fd05b2544016f2b804cbecbacbed22dd721af85
Chase <breannachase1@gmail.com>
2024-10-14 10:01:09 -0600

    v2 - adjusted the time to only run on Sundays, added a timeout system to restart anyways if the updates take longer than 10 minutes

--- Scheduler.ps1
+++ Scheduler.ps1
@@ -1,15 +1,23 @@
 
 # prompt the user to elevate and enter administrator credentials:
-Write-Host "Please make sure you have elevated and are running this script in a administrator command prompt!"
+Write-Host "This script creates a scheduled task for updating Amazon Windows computers and restarting them every Sunday at 5:30am."
+Write-Host "Please make sure you have elevated your permissions and are running this script in an administrator command prompt!"
+
+#hide the password from console view
 $pass = Read-Host -Prompt "Enter the password for $($Env:UserName)"
+
 $user = 'ANT\' + $Env:UserName
 
-Write-Host $User
-
 #specify the time the task will run
-$time = '5:10pm'
+$time = '5:30am'
+
+#run the script even if the computer is asleep or on battery power
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -WakeToRun
 
 # add a scheduled task to Windows Task Scheduler to run the Updater.ps1 file during the specified time
-$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-File C:\Windows\PowershellUpdateScript\Updater.ps1' -WorkingDirectory 'C:\Windows\PowershellUpdateScript'
-$trigger = New-ScheduledTaskTrigger -Daily -At $time 
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Windows Update Script" -Description "Updates and restarts Windows at a specified time every day" -RunLevel Highest -User $user -Password $pass
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-File C:\Users\chasbrec\Documents\Repos\PowershellUpdateScript\Updater.ps1' -WorkingDirectory 'C:\Windows\PowershellUpdateScript'
+$trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Sunday -At $time
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Windows Update Script" -Description "Updates and restarts Windows at a specified time every day" -Settings $settings -RunLevel Highest -User $user -Password $pass
+
+
+#-File C:\Users\Public\PowershellUpdateScript\Updater.ps1
--- Updater.ps1
+++ Updater.ps1
@@ -1,5 +1,35 @@
-#update windows then restart the computer
+#update windows
 
-Get-WindowsUpdate
-Install-WindowsUpdate -AcceptAll -AutoReboot
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show("The computer will update then restart in the next several minutes. Please save your work.", "Restart Warning", 'OK', 'Warning')
+Start-Sleep -Seconds 60
+
+$job = Start-Job -ScriptBlock {
+    Get-WindowsUpdate
+    Install-WindowsUpdate -AcceptAll -AutoReboot
+}
+
+#if the updates take longer than 10 minutes to run, force the computer to restart
+#after 9 minutes have passed, warn the user that the computer will restart in 1 minute
+$restarttimeout = 600 
+$warningtimeout = 540
+
+while ($job.State -eq 'Running') {
+    Start-Sleep -Seconds 60
+    if ($stopwatch.Elapsed.TotalSeconds -gt $warningtimeout) {
+        [System.Windows.Forms.MessageBox]::Show("The computer will restart in 1 minute. Please save your work.", "Restart Warning", 'OK', 'Warning')
+        
+    }
+
+    if ($stopwatch.Elapsed.TotalSeconds -gt $restarttimeout) {
+        Stop-Job -Job $job
+        Restart-Computer -Force
+    }
+}
+
+#if the computer requires no updates or has completed updates, restart anyways to log out stale user profiles
+[System.Windows.Forms.MessageBox]::Show("The computer will restart in 1 minute. Please save your work.", "Restart Warning", 'OK', 'Warning')
+Start-Sleep -Seconds 60
 Restart-Computer -Force
+
+
