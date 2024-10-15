a899083177baf73dd5e5e4bc233cd3ef6d82244b
Chase <breannachase1@gmail.com>
2024-10-15 13:47:02 -0600

    v2 - fixed an error with the incorrect file path

--- Scheduler.ps1
+++ Scheduler.ps1
@@ -3,7 +3,7 @@
 Write-Host "This script creates a scheduled task for updating Amazon Windows computers and restarting them every Sunday at 5:30am."
 Write-Host "Please make sure you have elevated your permissions and are running this script in an administrator command prompt!"
 
-#hide the password from console view
+#prompt for the password
 $pass = Read-Host -Prompt "Enter the password for $($Env:UserName)"
 
 $user = 'ANT\' + $Env:UserName
@@ -15,9 +15,6 @@ $time = '5:30am'
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -WakeToRun
 
 # add a scheduled task to Windows Task Scheduler to run the Updater.ps1 file during the specified time
-$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-File C:\Users\chasbrec\Documents\Repos\PowershellUpdateScript\Updater.ps1' -WorkingDirectory 'C:\Windows\PowershellUpdateScript'
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-File C:\Users\Public\PowershellUpdateScript\Updater.ps1' -WorkingDirectory 'C:\Windows\PowershellUpdateScript'
 $trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Sunday -At $time
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Windows Update Script" -Description "Updates and restarts Windows at a specified time every day" -Settings $settings -RunLevel Highest -User $user -Password $pass
-
-
-#-File C:\Users\Public\PowershellUpdateScript\Updater.ps1
